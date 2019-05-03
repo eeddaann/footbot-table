4c80dc5a3abee65700101f8d98fadf48f7982a6b
eeddaann <edan100@gmail.com>
2019-05-03 13:19:33 +0300

    engine WIP

--- engine.py
+++ engine.py
@@ -1,55 +1,70 @@
-import RPi.GPIO as GPIO
+#!/usr/bin/python
+# Import required libraries
+import sys
 import time
+import RPi.GPIO as GPIO
  
+# Use BCM GPIO references
+# instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-coil_A_1_pin = 4 # pink
-coil_A_2_pin = 17 # orange
-coil_B_1_pin = 23 # blue
-coil_B_2_pin = 24 # yellow
  
-# adjust if different
-StepCount = 8
-Seq = [None,None,None,None,None,None,None,None]
-Seq[0] = [0,1,0,0]
-Seq[1] = [0,1,0,1]
-Seq[2] = [0,0,0,1]
-Seq[3] = [1,0,0,1]
-Seq[4] = [1,0,0,0]
-Seq[5] = [1,0,1,0]
-Seq[6] = [0,0,1,0]
-Seq[7] = [0,1,1,0]
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+StepPins = [4,17,23,24]
  
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+# Set all pins as output
+for pin in StepPins:
+  print("Setup pins")
+  GPIO.setup(pin,GPIO.OUT)
+  GPIO.output(pin, False)
  
-GPIO.output(enable_pin, 1)
+# Define advanced sequence
+# as shown in manufacturers datasheet
+Seq = [[1,0,0,1],
+       [1,0,0,0],
+       [1,1,0,0],
+       [0,1,0,0],
+       [0,1,1,0],
+       [0,0,1,0],
+       [0,0,1,1],
+       [0,0,0,1]]
+        
+StepCount = len(Seq)
+StepDir = 1 # Set to 1 or 2 for clockwise
+            # Set to -1 or -2 for anti-clockwise
  
-def setStep(w1, w2, w3, w4):
-    GPIO.output(coil_A_1_pin, w1)
-    GPIO.output(coil_A_2_pin, w2)
-    GPIO.output(coil_B_1_pin, w3)
-    GPIO.output(coil_B_2_pin, w4)
+# Read wait time from command line
+if len(sys.argv)>1:
+  WaitTime = int(sys.argv[1])/float(1000)
+else:
+  WaitTime = 10/float(1000)
  
-def forward(delay, steps):
-    for i in range(steps):
-        for j in range(StepCount):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
+# Initialise variables
+StepCounter = 0
  
-def backwards(delay, steps):
-    for i in range(steps):
-        for j in reversed(range(StepCount)):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
+# Start main loop
+while True:
  
-if __name__ == '__main__':
-    while True:
-        delay = 500
-        steps = 10
-        forward(int(delay) / 1000.0, int(steps))
-        steps = 10
-        backwards(int(delay) / 1000.0, int(steps))
+  print(StepCounter)
+  print(Seq[StepCounter])
+ 
+  for pin in range(0, 4):
+    xpin = StepPins[pin]
+    if Seq[StepCounter][pin]!=0:
+      print(" Enable GPIO %i" %(xpin))
+      GPIO.output(xpin, True)
+    else:
+      GPIO.output(xpin, False)
+ 
+  StepCounter += StepDir
+ 
+  # If we reach the end of the sequence
+  # start again
+  if (StepCounter>=StepCount):
+    StepCounter = 0
+  if (StepCounter<0):
+    StepCounter = StepCount+StepDir
+ 
+  # Wait before moving on
+  time.sleep(WaitTime)
